095085538ab966f6201643ef5e5206d385fc3c5c
JabedH <jabedhtusar@gmail.com>
2022-09-25 11:12:29 +0600

    added Signup section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/identification/Login";
+import Reg from "./components/identification/Signup";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Reg />} />
+      </Routes>
     </div>
   );
 }
